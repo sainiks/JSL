6a26d2f51dbdf9296bb3ae6792c2adaed3a9f8e9
Kunal Saini <66879424+sainiks@users.noreply.github.com>
2025-12-05 21:22:07 +0000

    add BMI calculator and color switcher projects with HTML, CSS, and JavaScript

--- 07_Projects/BMI_Index/script.js
+++ 07_Projects/BMI_Index/script.js
@@ -1,43 +1,3 @@
-#project related to DOM
-
-#project link
-[click here]
-
-#Project - 1
-#Solution code 
-```javascript
-
-const buttons = document.querySelectorAll('.button')
-const body = document.querySelector("body")
-
-buttons.forEach(function(button){
-    console.log(button);
-    button.addEventListener('click',function(event_object){
-        console.log(event_object)
-        console.log(event_object.target)
-        if(event_object.target.id === "grey"){
-            body.style.backgroundColor= event_object.target.id
-        } 
-        if(event_object.target.id === "white"){
-            body.style.backgroundColor= event_object.target.id
-        } 
-        if(event_object.target.id === "blue"){
-            body.style.backgroundColor= event_object.target.id
-        } 
-        if(event_object.target.id === "yellow"){
-            body.style.backgroundColor= event_object.target.id
-        } 
-        if(event_object.target.id === "purple"){
-            body.style.backgroundColor= event_object.target.id
-        } 
-    })
-});
-
-```
-
-#Project - 2 Solution
-```javascript
-
 const form = document.querySelector("form")
 // this usecase will give empty values
 // const height = parseInt(document.querySelector('#height').value)
@@ -67,5 +27,3 @@ form.addEventListener('submit',function(event_object){
       }
     }
 });
-
-```
